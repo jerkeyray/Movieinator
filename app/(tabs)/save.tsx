70d6653a1ab1b5319038422b5c5a4f0b5f99c1ef
srivastavya02 <srivastavya24@gmail.com>
2025-05-17 11:46:25 +0530

    add icon and edit navbar

--- app/(tabs)/save.tsx
+++ app/(tabs)/save.tsx
@@ -1,7 +1,7 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
-const saved = () => {
+const save = () => {
   return (
     <View>
       <Text>saved</Text>
@@ -9,4 +9,4 @@ const saved = () => {
   )
 }
 
-export default saved
+export default save
