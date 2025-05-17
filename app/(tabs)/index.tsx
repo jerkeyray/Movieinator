33a5e7642e52a3ac55fb2162077516ff5c869d71
srivastavya02 <srivastavya24@gmail.com>
2025-05-17 11:28:41 +0530

    add navigation and design navbar

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,12 @@
+import { Link } from "expo-router";
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-5xl text-primary font-bold">Welcome</Text>
+      <Link href="/onboarding">Onboarding</Link>
+      
+    </View>
+  );
+}
